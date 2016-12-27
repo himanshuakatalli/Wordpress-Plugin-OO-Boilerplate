c82163771f367438277cebcacd569c9cf7b0bc06
Himanshu Singh <himanshuakatalli@users.noreply.github.com>
2016-12-27 12:17:23 +0530

    Update prefix-plugin.php

--- prefix-plugin.php
+++ prefix-plugin.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ Plugin Name: Your plugin name goes here
+ Plugin URI: Your plugin URI
+ Description: Your plugin description.
+ Author: Author name
+ Author URI: Author URI
+ License: Open License
+ */
 
 $plugin = require "./bootstrap/app.php";
 
